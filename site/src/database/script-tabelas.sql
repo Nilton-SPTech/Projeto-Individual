efd83619d42f6cb8ff557c29f42de3eb3f1f906f
Nilton-SPTech <nilton.gomes@sptech.school>
2022-11-13 13:12:55 -0300

    Adicionando o banco de dados

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -6,44 +6,50 @@
 comandos para mysql - banco local - ambiente de desenvolvimento
 */
 
-CREATE DATABASE aquatech;
+-- COMANDOS MYSQL PARA CRIAR O BANCO DE DADOS
 
-USE aquatech;
+CREATE DATABASE projetoIndividual; 
+USE projetoIndividual; 
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
+CREATE TABLE usuario(
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT, 
+	nome VARCHAR(45), 
+	email VARCHAR(50), 
+	senha VARCHAR(25), 
+	pais VARCHAR(20), CONSTRAINT chkPais CHECK(pais IN ('Brasil', 'EUA')), 
+	pontos INT
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+
+CREATE TABLE pokemon(
+	idPokemon INT PRIMARY KEY AUTO_INCREMENT, 
+	nome VARCHAR(45), 
+	tipoPrimario VARCHAR(45),
+	tipoSecundario VARCHAR(45),
+	vidaTotal INT,
+	descricao VARCHAR(60),
+	ataqueFraco VARCHAR(45), 
+	ataqueForte VARCHAR(45)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
+
+CREATE TABLE pokemonUsuario(
+	batalhas INT, 
+	fkUsuario INT, FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario), 
+	fkPokemon INT, FOREIGN KEY (fkPokemon) REFERENCES pokemon(idPokemon),
+	PRIMARY key(fkUsuario, fkPokemon)
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+CREATE TABLE imagemPokemon(
+	idImagemPokemon INT AUTO_INCREMENT, 
+	enderecoImagemFrente VARCHAR(50),
+	enderecoImagemCosta VARCHAR(50),
+	enderecoGif VARCHAR(50),
+	fkPokemon INT UNIQUE, FOREIGN KEY (fkPokemon) REFERENCES pokemon(idPokemon),
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+	PRIMARY KEY(idImagemPokemon, fkPokemon)
 );
+
 
 
 /*
